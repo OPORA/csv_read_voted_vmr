8fbe174bdece68bc28e13836f5aa62ff2b7e939b
Beastie PC <gskondratenko@opora.org.ua>
2017-04-25 07:55:44 +0300

    Remove rar file

--- lib/rar.rb
+++
@@ -1,7 +0,0 @@
-require 'unrar'
-archive = Unrar.new('81.rar')
-# file = archive.list_contents.first[:filename]
-# p archive.extract(file)
-file_id = archive.list_contents.first[:filename]
-
-File.open('output-name.rtf', 'w') { |file| file.write(archive.extract(file_id)) }

8c72da9b2e01f85a5df47ed585a8079b05dc1de3
Beastie PC <gskondratenko@opora.org.ua>
2018-02-12 00:45:42 +0200

    Change select id mp and added new style voted

--- lib/scrape_all_votes.rb
+++ lib/scrape_all_votes.rb
@@ -1,11 +1,12 @@
 require_relative 'voted'
 require_relative 'get_mps'
 require 'json'
+require 'date'
 
 class GetAllVotes
    def initialize
      @all_file = get_all_file()
-     $all_mp =  GetMp.new
+     #$all_mp =  GetMp.new
    end
    def get_all_file
      hash = []
@@ -26,15 +27,16 @@ class GetAllVotes
     file = open(json).read
 
     my_hash = JSON.parse(file)
-
-    date_caden = Date.parse(my_hash["sessionDate"],'%d.%m.%Y')
+    p my_hash["sessionDate"]
+    date_caden = Date.strptime(my_hash["sessionDate"],'%d.%m.%y')
     rada_id = 6
 
     my_hash["voting"].each_with_index  do |v, i|
       next if v["namedVoting"].empty?
       name = v["voteName"].strip
       number = i + 1
-      date_vote =  v["voteTimestamp"]
+       p v["voteTimestamp"]
+      date_vote =  DateTime.strptime(v["voteTimestamp"], '%d.%m.%y %H:%M:%S')
       event = VoteEvent.first(name: name, date_vote: date_vote, number: number, date_caden: date_caden, rada_id: rada_id)
       if event.nil?
         events = VoteEvent.new(name: name, date_vote: date_vote, number: number, date_caden: date_caden, rada_id: rada_id)
@@ -49,9 +51,8 @@ class GetAllVotes
       ages =[]
       v["namedVoting"].each do |r|
         v = r.to_a[0]
-        next if v.first == "Бичковяк Олена Вікторівна" or  v.first == "Кучма Тарас Ярославович" or  v.first == "Городинський Михайло Мар\"янович" or v.first == "Городинський Михайло Мар`янович"
         vote = events.votes.new
-        vote.voter_id = $all_mp.serch_mp(v.first)
+        vote.voter_id = v.first #$all_mp.serch_mp(v.first)
         vote.result =  short_voted_result(v.last)
         vote.save
 
